ce7f1201272a5b69d0a27cc991445e32275da4cd
nearlynithin <nearlynithin@gmail.com>
2024-07-10 23:57:51 +0530

    ✨ feat: Can print characters but there's distortion in the image, Needs a better algorithm to print

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	_ "image/png"
 	"log"
 	"os"
-	"strconv"
 )
 
 func main() {
@@ -15,7 +14,7 @@ func main() {
 		log.Fatal(err)
 	}
 	defer test_img.Close()
-
+	
 	imgData, _, err := image.Decode(test_img)
 	if err != nil {
 		log.Fatal(err)
@@ -30,21 +29,33 @@ func main() {
 	}
 	defer f.Close()
 
-	for y := bound.Min.Y; y < bound.Max.Y; y++ {
-		for x := bound.Min.X; x < bound.Max.X; x++ {
+	heightscale:=2
+	for y := bound.Min.Y; y < bound.Max.Y; y+=heightscale {
+		for x := bound.Min.X; x < bound.Max.X; x++{
 			oldPixel := imgData.At(x, y)
 			pixel := color.GrayModel.Convert(oldPixel)
 			imgSet.Set(x, y, pixel)
 			c := imgSet.At(x, y)
-			r, g, b, a := c.RGBA()
+			r, g, b, _ := c.RGBA()
 			r8 := uint8(r >> 8)
 			g8 := uint8(g >> 8)
 			b8 := uint8(b >> 8)
-			a8 := uint8(a >> 8)
-			_, err := f.WriteString(strconv.Itoa(int(r8)) + " " + strconv.Itoa(int(g8)) + " " + strconv.Itoa(int(b8)) + " " + strconv.Itoa(int(a8)) + "\n")
-			if err != nil {
-				log.Printf("Error writing RGB values: %v", err)
+			// a8 := uint8(a >> 8)
+			
+			intensity := (r8+g8+b8/3)
+			switch {
+			case intensity>= 255:
+				f.WriteString("@")
+			case intensity>=191:
+				f.WriteString("%")
+			case intensity>=127:
+				f.WriteString("#")
+			case intensity>=64:
+				f.WriteString("$")
+			default:
+				f.WriteString("   ")
 			}
 		}
+		f.WriteString("\n")
 	}
 }
